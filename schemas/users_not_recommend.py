4d84c0d81e2dd52199420bfeca2afd94a6fa5252
Elí Flores <eli1998flores@gmail.com>
2023-10-27 01:32:20 -0400

    try fix

--- schemas/users_not_recommend.py
+++ schemas/users_not_recommend.py
@@ -1,6 +1,6 @@
-from pydantic import RootModel
+from pydantic import root_model
 
-class UsersNotRecommend_(RootModel):
+class UsersNotRecommend_(root_model):
   root: dict
   def __getitem__(self, item):
     return getattr(self, item)
